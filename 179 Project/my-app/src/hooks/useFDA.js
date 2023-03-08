c94eb0171770065d8f70f56c4d19d251ca9f6f70
Spencer Barns <smbarns113@gmail.com>
2023-03-08 15:23:55 -0800

    Basics for FDA pages and hook creation

--- 179 Project/my-app/src/hooks/useFDA.js
+++ 179 Project/my-app/src/hooks/useFDA.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import { createVendiaClient } from '@vendia/client'
 
-export default function useFDA()
-{
-    return (<div>useFDA</div>);
+const client = createVendiaClient({
+  apiUrl: `https://8ox2axykv8.execute-api.us-west-2.amazonaws.com/graphql/`,
+  websocketUrl: `wss://cyua4epxl0.execute-api.us-west-2.amazonaws.com/graphql`,
+  apiKey: 'FVGkPyD9HcrLBbFC4DmFDM1attWQFieLzyqtymoKQVwZ', //real world dont put this here its public
+})
+
+const {entities} = client;
+
+const usemyFDA = () => {
+    return {entities}
+}
+export default function usemyFDA() {
+    return(
+        <div>usemyFDA</div>
+    )
 }
